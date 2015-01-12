8586eeba8cb98d61120634a0701d76883f03314c
Laurie Clark-Michalek <lclarkmichalek@gmail.com>
2015-01-12 00:18:42 +0000

    Add many tests for controller and friends

--- tests/Tests/HighHock/Controller.hs
+++ tests/Tests/HighHock/Controller.hs
@@ -2,11 +2,14 @@ module Tests.HighHock.Controller (controllerTests) where
 
 import Test.Hspec
 import Control.Monad (void, forM, forM_)
-import Control.Concurrent.Async (async, poll)
+import qualified Control.Concurrent.Async as A
 import Control.Concurrent (threadDelay)
-import Data.Maybe (isJust)
+import Control.Exception (SomeException)
 
 import Network.HighHock.Controller
+
+instance Eq SomeException where
+  (==) a b = show a == show b
 
 controllerStates :: IO [Controller]
 controllerStates = do
@@ -17,17 +20,19 @@ controllerStates = do
   void $ trigger triggered
   return [empty, stopped, triggered]
 
+timeout :: Int
+timeout = 10 ^ 4
+
 controllerTests = do
   describe "Controller.stop" $ do
     it "stops the controller immedietly" $ do
-      let timeout = 10 ^ 3 -- Nanos
       states <- controllerStates
-      asyncs <- forM states $ \c -> async $ do
+      asyncs <- forM states $ \c -> A.async $ do
         stop c
       threadDelay timeout
       forM_ asyncs $ \a -> do
-        state <- poll a
-        state `shouldSatisfy` isJust
+        state <- A.poll a
+        state `shouldBe` Just (Right ())
 
   describe "Controller.trigger" $ do
     it "does not trigger a stopped controller" $ do
@@ -37,3 +42,61 @@ controllerTests = do
       t `shouldBe` False
       r <- wait c
       r `shouldBe` False
+
+    it "does trigger an empty or triggered controller" $ do
+      c <- newController
+      t <- trigger c
+      t `shouldBe` True
+      t' <- trigger c
+      t' `shouldBe` True
+
+  describe "Controller.wait" $ do
+    it "waits on a trigger" $ do
+      c <- newController
+      a <- A.async $ wait c
+      threadDelay timeout
+      void $ trigger c
+      threadDelay timeout
+      r <- A.poll a
+      r `shouldBe` Just (Right True)
+
+    it "waits on a stop" $ do
+      c <- newController
+      a <- A.async $ wait c
+      threadDelay timeout
+      stop c
+      threadDelay timeout
+      r <- A.poll a
+      r `shouldBe` Just (Right False)
+
+    it "does not consume a stop" $ do
+      c <- newController
+      a <- A.async $ wait c
+      b <- A.async $ wait c
+      stop c
+      threadDelay timeout
+      r <- A.poll a
+      r `shouldBe` Just (Right False)
+      r' <- A.poll b
+      r' `shouldBe` Just (Right False)
+
+  describe "Controller.ticker" $ do
+    it "stops on a stop" $ do
+      c <- newController
+      a <- A.async $ ticker c (timeout `div` 4)
+      threadDelay timeout
+      stop c
+      threadDelay timeout
+      r <- A.poll a
+      r `shouldBe` Just (Right ())
+
+    it "should cause triggers periodically" $ do
+      c <- newController
+      a <- A.async $ ticker c (timeout `div` 4)
+      threadDelay timeout
+      v <- wait c
+      v `shouldBe` True
+      threadDelay timeout
+      v' <- wait c
+      v' `shouldBe` True
+      A.cancel a
