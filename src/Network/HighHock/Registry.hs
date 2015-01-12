4e6688affbe2387798f36645a99701d70ea8d6e5
Laurie Clark-Michalek <lclarkmichalek@gmail.com>
2015-01-12 01:49:00 +0000

    Change order of registry arguments to make chaing possible

--- src/Network/HighHock/Registry.hs
+++ src/Network/HighHock/Registry.hs
@@ -25,43 +25,43 @@ type Registry = M.Map T.Text C.Controller
 newRegistry :: Registry
 newRegistry = M.empty
 
-applyContainer :: (C.Controller -> IO a) -> Registry -> T.Text -> IO (Maybe a)
-applyContainer f r id = case M.lookup id r of
+applyContainer :: (C.Controller -> IO a) -> T.Text -> Registry -> IO (Maybe a)
+applyContainer f id r = case M.lookup id r of
   Just c -> fmap Just $ f c
   Nothing -> return Nothing
 
 -- | Inserts a new container to be watched. If there is already an entry for the
 -- container, will stop it
-insertContainer :: Registry -> (C.Controller -> IO ()) -> T.Text -> IO Registry
-insertContainer r act id = do
+insertContainer :: (C.Controller -> IO ()) -> T.Text -> Registry -> IO Registry
+insertContainer act id r = do
   c <- C.newController
-  applyContainer C.stop r id
+  applyContainer C.stop id r
   void $ CC.forkIO $ finally (act c) (C.stop c)
   infoM "insertContainer" ("Inserting " ++ T.unpack id)
   return $ M.insert id c r
 
-removeContainer :: Registry -> T.Text -> IO Registry
-removeContainer r id = do
-  applyContainer C.stop r id
+removeContainer :: T.Text -> Registry -> IO Registry
+removeContainer id r = do
+  applyContainer C.stop id r
   infoM "removeContainer" ("Removing " ++ T.unpack id)
   return $ M.delete id r
 
 -- | Remove containers that are missing from the list of ids given. Stop also
-removeMissingContainers :: Registry -> [T.Text] -> IO Registry
-removeMissingContainers r ids = foldM removeContainer r missing
+removeMissingContainers :: [T.Text] -> Registry -> IO Registry
+removeMissingContainers ids r = foldM (flip removeContainer) r missing
   where missing = filter (`notElem` ids) $ M.keys r
 
 -- | Add containers that are missing from the registry (and in the list of ids given)
-insertMissingContainers :: Registry -> (T.Text -> C.Controller -> IO ()) ->
-                           [T.Text] -> IO Registry
-insertMissingContainers r fact ids = foldM insert r missing
+insertMissingContainers :: (T.Text -> C.Controller -> IO ()) -> [T.Text]
+                           -> Registry -> IO Registry
+insertMissingContainers fact ids r = foldM insert r missing
   where missing = filter (`notElem` M.keys r) ids
-        insert r' i = insertContainer r' (fact i) i
+        insert r' i = insertContainer (fact i) i r'
 
 -- | Check the controller of each entry, and remove if stopped (any thread that
 -- finish will be stopped, due to our finally wrapper in the forkIO)
 removeStoppedContainers :: Registry -> IO Registry
-removeStoppedContainers r = dead >>= foldM removeContainer r
+removeStoppedContainers r = dead >>= foldM (flip removeContainer) r
   where dead = M.foldlWithKey acc (return []) r
         acc :: IO [T.Text] -> T.Text -> C.Controller -> IO [T.Text]
         acc v id c = do
--- tests/Tests/HighHock/Registry.hs
+++ tests/Tests/HighHock/Registry.hs
@@ -18,21 +18,19 @@ sleep :: a -> IO ()
 sleep _ = threadDelay $ 10 ^ 6
 
 mkReg :: [T.Text] -> IO Registry
-mkReg (x:xs) = do
-  r' <- mkReg xs
-  insertContainer r' sleep x
+mkReg (x:xs) = mkReg xs >>= insertContainer sleep x
 mkReg [] = return newRegistry
 
 registryTests = do
   describe "Registry.insertContainer" $ do
     it "will add a missing container" $ do
-      reg <- insertContainer newRegistry sleep "id"
+      reg <- insertContainer sleep "id" newRegistry
       M.lookup "id" reg `shouldSatisfy` isJust
       M.size reg `shouldBe` 1
 
     it "will run the given action" $ do
       let act c = (threadDelay $ 10 ^ 5) >> C.stop c
-      reg <- insertContainer newRegistry act "id"
+      reg <- insertContainer act "id" newRegistry
       a <- A.async $ C.wait (reg M.! "id")
       threadDelay $ 10 ^ 4
       r <- A.poll a
@@ -42,8 +40,8 @@ registryTests = do
       r' `shouldBe` Just (Right False)
 
     it "will stop overwritten entries" $ do
-      reg <- insertContainer newRegistry sleep "id"
-      reg' <- insertContainer reg sleep "id"
+      reg <- insertContainer sleep "id" newRegistry
+      reg' <- insertContainer sleep "id" reg
       s <- C.isStopped $ reg M.! "id"
       s `shouldBe` True
       s' <- C.isStopped $ reg' M.! "id"
@@ -51,44 +49,44 @@ registryTests = do
 
     it "will handle exceptions in the action" $ do
       let act _ = (threadDelay $ 10 ^ 2) >>= fail "foo"
-      reg <- insertContainer newRegistry act "id"
+      reg <- insertContainer act "id" newRegistry
       threadDelay $ 10 ^ 4
       s <- C.isStopped $ reg M.! "id"
       s `shouldBe` True
 
   describe "Registry.removeContainer" $ do
     it "will ignore missing entries" $ do
-      reg <- insertContainer newRegistry sleep "id"
-      r' <- removeContainer reg "asd"
+      reg <- insertContainer sleep "id" newRegistry
+      r' <- removeContainer "asd" reg
       M.size r' `shouldBe` 1
 
     it "will remove entries" $ do
-      reg <- insertContainer newRegistry sleep "id"
-      r' <- removeContainer reg "id"
+      reg <- insertContainer sleep "id" newRegistry
+      r' <- removeContainer "id" reg
       M.size r' `shouldBe` 0
 
     it "will stop removed entries" $ do
-      reg <- insertContainer newRegistry sleep "id"
-      void $ removeContainer reg "id"
+      reg <- insertContainer sleep "id" newRegistry
+      void $ removeContainer "id" reg
       s <- C.isStopped $ reg M.! "id"
       s `shouldBe` True
 
   describe "Registry.removeMissingContainers" $ do
     it "will remove missing containers" $ do
       reg <- mkReg ["asd", "bsd"]
-      reg' <- removeMissingContainers reg ["asd", "nsd"]
+      reg' <- removeMissingContainers ["asd", "nsd"] reg
       M.keys reg' `shouldBe` ["asd"]
 
   describe "Registry.insertMissingContainers" $ do
     it "will add missing containers" $ do
       reg <- mkReg ["asd", "bsd"]
-      reg' <- insertMissingContainers reg (\_ _ -> threadDelay $ 10 ^ 6) ["asd", "nsd"]
+      reg' <- insertMissingContainers (\_ _ -> threadDelay $ 10 ^ 6) ["asd", "nsd"] reg
       M.keys reg' `shouldMatchList` ["asd", "bsd", "nsd"]
 
   describe "Registry.removeStoppedContainers" $ do
     it "will remove naturally containers" $ do
       let act _ = threadDelay $ 10 ^ 2
-      reg <- insertContainer newRegistry act "id"
+      reg <- insertContainer act "id" newRegistry
       threadDelay $ 10 ^ 4
       r' <- removeStoppedContainers reg
       M.keys r' `shouldMatchList` []
@@ -106,7 +104,7 @@ registryTests = do
 
     it "will remove thrown containers" $ do
       let act _ = (threadDelay $ 10 ^ 2) >>= fail "foo"
-      reg <- insertContainer newRegistry act "id"
+      reg <- insertContainer act "id" newRegistry
       threadDelay $ 10 ^ 4
       r' <- removeStoppedContainers reg
       M.keys r' `shouldMatchList` []
